33be749a7fb3779eda8c0b88d998ad08e359ecbf
螺丝钉 <32859363+gyh03@users.noreply.github.com>
2017-11-01 16:40:39 +0800

    provider xml

--- myDubbox-provider/src/main/java/main/Main.java
+++ myDubbox-provider/src/main/java/main/Main.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"provider.xml","spring-mvc.xml"});
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"dubbo-demo-provider.xml","spring-mvc.xml"});
         context.start();
 
         System.in.read(); 
